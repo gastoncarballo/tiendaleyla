ef4d5461f73f20d5a8434a2084d248648518a7a4
gastoncarballo <sgcarballo1978@gmail.com>
2023-03-30 21:58:48 -0300

    tiendaleyla

---
+++ app.js
@@ -0,0 +1,21 @@
+// MENU LATERAL
+var menu_visible = false;
+let menu = document.getElementById("nav");
+function mostrarOcultarMenu(){
+    if(menu_visible==false){
+        menu.style.display = "block";
+        menu_visible = true;
+    }
+    else{
+        menu.style.display = "none";
+        menu_visible = false;
+    }
+}
+// OCULTO EL MENU UNA VEZ QUE SELECCIONO UNA OPCION
+let links = document.querySelectorAll("nav a");
+for(var x = 0; x <links.length;x++){
+    links[x].onclick = function(){
+        menu.style.display = "none";
+        menu_visible = false;
+    }
+}
